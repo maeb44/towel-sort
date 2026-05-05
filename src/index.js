c930d1f37e26592c0ad845f8deca8f394c3e01d3
maeb44 <denchik2004@yandex.ru>
2026-05-05 18:02:36 +0300

    feat:add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix = []) {
+  const arr1 = [];
+  for (let i = 0; i < matrix.length; i += 1) {
+    const element = matrix[i];
+    if (i % 2 !== 0) {
+      arr1.push(...element.reverse());
+    } else {
+      arr1.push(...element);
+    }
+  }
+  return arr1;
 };
